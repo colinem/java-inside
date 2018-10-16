ec471c73c81771b0d5daccdd3a17fe2875028c43
coline <coline_mignot@hotmail.fr>
2018-10-16 11:00:35 +0200

    continuation

--- labfour/src/main/java/fr.umlv.javainside.labfour/fr/umlv/javainside/labfour/Exemple1.java
+++ labfour/src/main/java/fr.umlv.javainside.labfour/fr/umlv/javainside/labfour/Exemple1.java
@@ -1,13 +1,16 @@
 package fr.umlv.javainside.labfour;
 
-public class Exemple1 {
+public class Exemple1 {	
 
 	public static void main() {
 		
+		Object lock = new Object();
 		var s = new ContinuationScope("hello1");
 		Continuation c = new Continuation(s, 
 				() -> {
-					Continuation.yield(s);
+					synchronized(lock) { // IllegalSateException : peux pas sortir du synchronized avec un yield sinon tu sors avec ton token c'est pas bon
+						Continuation.yield(s);
+					}
 					System.out.println("hello continuation");
 				});
 		c.run();
