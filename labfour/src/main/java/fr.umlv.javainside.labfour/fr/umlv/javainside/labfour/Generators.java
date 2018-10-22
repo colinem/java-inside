b2ec639a10218012a300b8656088f55ba56ee4dd
coline <coline_mignot@hotmail.fr>
2018-10-22 15:26:39 +0200

    labfour fin ex1

---
+++ labfour/src/main/java/fr.umlv.javainside.labfour/fr/umlv/javainside/labfour/Generators.java
@@ -0,0 +1,14 @@
+package fr.umlv.javainside.labfour;
+
+import java.util.Iterator;
+
+import fr.umlv.javainside.labfour.Exemple1.Scheduler;
+
+public class Generators<T> {
+	
+	Iterator<T> generateIterator(Scheduler s) {
+		
+		return null;
+	}
+	
+}
